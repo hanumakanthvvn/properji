c2250602ee746d164c0e16fbc7c7d053e4e57397
Hanumakanth <hanumakanth.v@gmail.com>
2013-07-09 18:19:37 +0530

    assets precompiled made false

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -12,8 +12,8 @@ Properji::Application.configure do
   config.serve_static_assets = false
 
   # Compress JavaScripts and CSS
-  config.assets.compress = true
-  config.assets.precompile += %w( gmaps4rails.css )
+  config.assets.compress = false
+  #config.assets.precompile += %w( gmaps4rails.css gmaps4rails.js )
   # Don't fallback to assets pipeline if a precompiled asset is missed
   config.assets.compile = false
 
